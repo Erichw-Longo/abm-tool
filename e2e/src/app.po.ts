3dd07b905172e1ace09925ae3ffeeb004ee4fd99
Erich Longo <erichw.longo@gmail.com>
2021-01-06 22:48:45 -0500

    Initial project setup.

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('amb-home-page .content span')).getText() as Promise<string>;
   }
 }
